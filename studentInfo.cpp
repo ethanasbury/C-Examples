05572d9e72e2cb65992edb540b0a51c0e6068139
Ethan Asbury <ethanasbury@gmail.com>
2015-06-19 11:52:20 -0700

    Create studentInfo.cpp

---
+++ studentInfo.cpp
@@ -0,0 +1,35 @@
+//Program Id:
+//Author:
+//Class:
+//Date Written:
+//Brief Description:
+//		A program that displays a students name, major, and
+//		classes the student is taking this semester
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+
+	string studentName = "Ethan Asbury";
+	string studentMajor = "Electrical Engineering";
+	string studentOtherClasses = "Ecn 212, Acc 111, Mat 221";
+
+	cout << "Student's Name: " << studentName <<endl;
+	cout << "Student's Major: " << studentMajor <<endl;
+	cout << "Student's Other Classes: " << studentOtherClasses <<endl;
+
+	return 0;
+}
+
+/* Expected Output
+
+Student's Name: Ethan Asbury
+Student's Major: Electrical Engineering
+Student's Other Classes: Ecn 212, Acc 111, Mat 221
+Press any key to continue . . .
+
+*/
